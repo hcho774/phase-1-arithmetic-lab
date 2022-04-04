e1b6bd98abbac3266bfa7a37d8d04403eb13ad60
Hyun Sung Cho <hcho774@gmail.com>
2022-04-04 16:48:31 -0400

    commit tree 9d2d11fe52808ef90fba5f2fc11fb13d5f4e9a9b

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 24;
+let num4 = 5;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;;
+let mod =  num3 % num4;
+const max = Math.max(1,2,3,20);
+
 
 // Write your code here
+
+/*
+const chai = require('chai')
+const expect = chai.expect;
+
+describe('index.js', function() {
+  describe('multiply', function() {
+    it("is an equation whose multiplied results will equal 62", function() {
+      expect(num1).to.be.a('number')
+      expect(num2).to.be.a('number')
+      expect(multiply).to.eq(62)
+    });
+  });
+  describe('random', function() {
+    it("generates a random integer greater than 0", function() {
+      expect(Number.isInteger(random)).to.be.true
+      expect(random).to.not.eq(0)
+    });
+  });
+  describe('mod', function() {
+    it("is an equation that calculates a remainder that is equal to 4", function() {
+      expect(num3).to.be.a('number')
+      expect(num4).to.be.a('number')
+      expect(mod).to.eq(4)
+    });
+  });
+  describe('max', function() {
+    it("will return 20 as the highest number in the set", function() {
+      expect(max).to.eq(20)
+    });
+  });
+});
+*/ 
